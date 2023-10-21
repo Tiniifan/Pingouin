9d0ad6b4262b442416c173effa07cee40a69510a
Tinifan <30804632+Tiniifan@users.noreply.github.com>
2023-10-21 02:01:40 +0200

    Push 1.3
    - add color when you replace/add files
    - add xpck support
    - can import xfsa or xpck on another xfsa or xpck
    - can replace one file

---
+++ ArchiveL5/Level5/Archive/IArchive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArchiveL5.Tools;
+
+namespace ArchiveL5.Level5.Archive
+{
+    public interface IArchive
+    {
+        string Name { get; }
+
+        VirtualDirectory Directory { get; set; }
+
+        void Save(string path, ProgressBar progressBar);
+
+        void Close();
+    }
+}
